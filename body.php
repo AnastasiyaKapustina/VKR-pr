9e8440a81d35b4191ae58707110eabe023f07778
AnastasiyaKapustina <79342504+AnastasiyaKapustina@users.noreply.github.com>
2023-01-18 16:10:34 +0900

    Initial commit

---
+++ body.php
@@ -0,0 +1,213 @@
+
+   <div class="main main-raised">
+        <div class="container mainn-raised" style="width:100%;">
+  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+   
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active">
+        <img src="img/banner3.jpg" alt="banner3" style="width:100%;">
+        
+      </div>
+
+      <div class="item">
+        <img src="img/banner2.jpg" alt="banner2" style="width:100%;">
+        
+      </div>
+    
+      <div class="item">
+        <img src="img/banner4.jpg" alt="banner3" style="width:100%;">
+        
+      </div>
+      <div class="item">
+        <img src="img/banner1.jpg" alt="banner1" style="width:100%;">
+        
+
+  
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control _26sdfg" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only" >Следующий</span>
+    </a>
+    <a class="right carousel-control _26sdfg" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Предыдущий</span>
+    </a>
+  </div>
+</div>
+     
+
+
+		<!-- SECTION -->
+		<div class="section mainn mainn-raised">
+		
+		
+			<!-- container -->
+			<div class="container">
+			
+				<!-- row -->
+				<div class="row">
+					<!-- shop -->
+					<div class="col-md-4 col-xs-6">
+						<a href="product.php?p=1"><div class="shop">
+							<div class="shop-img">
+								<img src="./img/shop01.png" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>Новое<br>поступление</h3>
+								<a href="product.php?p=1" class="cta-btn">перейти <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+						</div></a>
+					</div>
+					<!-- /shop -->
+
+					<!-- shop -->
+					<div class="col-md-4 col-xs-6">
+						<a href="product.php?p=10"><div class="shop">
+							<div class="shop-img">
+								<img src="./img/shop03.jpg" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>Коллекция<br>верхней одежды</h3>
+								<a href="product.php?p=10" class="cta-btn">перейти <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+						</div></a>
+					</div>
+					<!-- /shop -->
+
+					<!-- shop -->
+					<div class="col-md-4 col-xs-6">
+						<a href="product.php?p=79"><div class="shop">
+							<div class="shop-img">
+								<img src="./img/product01.png" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>Трикотаж<br></h3>
+								<a href="product.php?p=47" class="cta-btn">перейти <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+                            </div></a>
+					</div>
+					<!-- /shop -->
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /SECTION -->
+		  
+		
+
+		<!-- SECTION -->
+		<div class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+
+					<!-- section title -->
+					<div class="col-md-12">
+						<div class="section-title">
+							<h3 class="title">Новое поступление</h3>
+							
+						</div>
+					</div>
+					<!-- /section title -->
+
+					<!-- Products tab & slick -->
+					<div class="col-md-12 mainn mainn-raised">
+						<div class="row">
+							<div class="products-tabs">
+								<!-- tab -->
+								<div id="tab1" class="tab-pane active">
+									<div class="products-slick" data-nav="#slick-nav-1" >
+									
+									<?php
+                    include 'db.php';
+								
+                    
+					$product_query = "SELECT * FROM products,categories WHERE product_cat=cat_id AND product_id BETWEEN 1 AND 5";
+                $run_query = mysqli_query($con,$product_query);
+                if(mysqli_num_rows($run_query) > 0){
+
+                    while($row = mysqli_fetch_array($run_query)){
+                        $pro_id    = $row['product_id'];
+                        $pro_cat   = $row['product_cat'];
+                        $pro_brand = $row['product_brand'];
+                        $pro_title = $row['product_title'];
+                        $pro_price = $row['product_price'];
+                        $pro_image = $row['product_image'];
+
+                        $cat_name = $row["cat_title"];
+
+                        echo "
+				
+                        
+                                
+								<div class='product'>
+									<a href='product.php?p=$pro_id'><div class='product-img'>
+										<img src='product_images/$pro_image' style='max-height: 170px;' alt=''>
+
+									</div></a>
+									<div class='product-body'>
+										<p class='product-category'>$cat_name</p>
+										<h3 class='product-name header-cart-item-name'><a href='product.php?p=$pro_id'>$pro_title</a></h3>
+										<h4 class='product-price header-cart-item-info'>$pro_price<a class='product-old-price'> руб</a></h4>
+										<div class='product-rating'>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+											<i class='fa fa-star'></i>
+										</div>
+										<div class='product-btns'>
+											<button class='add-to-wishlist'><i class='fa fa-heart-o'></i><span class='tooltipp'>в список желаемого</span></button>
+											<button class='add-to-compare'><i class='fa fa-exchange'></i><span class='tooltipp'>сравнить</span></button>
+											<button class='quick-view'><i class='fa fa-eye'></i><span class='tooltipp'>быстрый просмотр</span></button>
+										</div>
+									</div>
+									<div class='add-to-cart'>
+										<button pid='$pro_id' id='product' class='add-to-cart-btn block2-btn-towishlist' href='#'><i class='fa fa-shopping-cart'></i> в корзину</button>
+									</div>
+								</div>
+                               
+							
+                        
+			";
+		}
+        ;
+      
+}
+?>
+										<!-- product -->
+										
+	
+										<!-- /product -->
+										
+										
+										<!-- /product -->
+									</div>
+									<div id="slick-nav-1" class="products-slick-nav"></div>
+								</div>
+								<!-- /tab -->
+							</div>
+						</div>
+					</div>
+					<!-- Products tab & slick -->
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /SECTION -->
+
+		
+		
+
+</div>
+		
